d91b8cb0cbc37b8d3ac04261545cc8886d19adc2
Mattia311 <mattia.space98@gmail.com>
2021-12-02 16:15:00 +0100

    secondo push

--- snake2/index.php
+++ snake2/index.php
@@ -7,5 +7,36 @@ $age = $_GET['age'];
 
 var_dump($name . $mail . $age);
 
+if(strlen($name) >= 4 && strpos($mail,"@") && strpos($mail,".") && is_numeric($age)){
+    $result ="Accesso riuscito";
+}else{
+    $result ="Accesso negato";
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./index.php" method= 'get'>
+        <label for="name">name </label>
+        <input type="text" name='name' id='name'>
+    </form>
+    <form action="./index.php" method= 'get'>
+        <label for="mail">Email </label>
+        <input type="text" name='mail' id='mail'>
+    </form>
+    <form action="./index.php" method= 'get'>
+        <label for="age">age </label>
+        <input type="text" name='age' id='age'>
+    </form>
+    <h1><?php echo $result ?></h1>
+    
+</body>
+</html>
